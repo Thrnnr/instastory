c5b2195aa6b245d8243f60b46fc98c0c98ea56c5
RuveTa <tahir_inanir@windowslive.com>
2021-12-22 17:41:08 +0300

    ilk commit

---
+++ app/src/main/java/com/ruveta/kotlininstagramprojectyeni/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ruveta.kotlininstagramprojectyeni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ruveta.kotlininstagramprojectyeni.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+    }
+}
